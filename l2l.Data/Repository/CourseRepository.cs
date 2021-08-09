e6a0de700286fc8d4dc5c19f48f12b98cef16c70
Gergely Vaczi <48059107+vaczigergely@users.noreply.github.com>
2021-08-09 18:47:00 +0200

    CourseRepository Prototype 1

--- l2l.Data/Repository/CourseRepository.cs
+++ l2l.Data/Repository/CourseRepository.cs
@@ -1,17 +1,26 @@
 using System;
+using l2l.Data.Model;
 
 namespace l2l.Data.Repository
 {
     public class CourseRepository
     {
+        private readonly L2lDbContext db;
+
+        public CourseRepository()
+        {
+            var factory = new L2lDbContextFactory();
+            db = factory.CreateDbContext(new string[] {});
+        }
+
         public void Add(Course entity)
         {
-
+            db.Courses.Add(entity);
         }
 
         public Course GetById(int Id)
         {
-            throw new NotImplementedException();
+            return db.Courses.Find(Id);
         }
     }
 }
