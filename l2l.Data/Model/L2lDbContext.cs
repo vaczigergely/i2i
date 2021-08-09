9dd80a2c29ce98b849e5861d642e694dd23c8665
Gergely Vaczi <48059107+vaczigergely@users.noreply.github.com>
2021-08-09 15:27:07 +0200

    #2: EntityFrameworkCore parameters

--- l2l.Data/Model/L2lDbContext.cs
+++ l2l.Data/Model/L2lDbContext.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace l2l.Data.Model
 {
     public class L2lDbContext : DbContext
     {
+        public L2lDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
         public DbSet<Course> Courses { get; set; }
     }
 }
