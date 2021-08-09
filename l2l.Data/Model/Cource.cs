9896c08b29bd7dd47a70e49c6b2fc5991a0040f3
Gergely Vaczi <48059107+vaczigergely@users.noreply.github.com>
2021-08-09 18:15:57 +0200

    Update Cource.cs

--- l2l.Data/Model/Cource.cs
+++ l2l.Data/Model/Cource.cs
@@ -1,39 +1,7 @@
 using System;
 
-public class Course : IEquatable<Course>
+public class Course
 {
     public int Id { get; set; }
     public string Name { get; set; }
-
-    public override bool Equals(object obj)
-    {
-        return Equals(obj as Course);
-    }
-
-    public bool Equals(Course course)
-    {
-        if (null == course)
-        {
-            return false;
-        }
-
-        if (Id != course.Id || Name != course.Name)
-        {
-            return false;
-        }
-
-        return true;
-    }
-
-    public override int GetHashCode()
-    {
-        unchecked
-        {
-            var hash = 27;
-            hash = (13 * hash) + Id.GetHashCode();
-            hash = (13 * hash) + Name.GetHashCode();
-
-            return hash; 
-        }
-    }
 }
